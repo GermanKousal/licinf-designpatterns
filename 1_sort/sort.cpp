8533487ccdfcd9b3d8c282a876ff1848f3e83d3a
Germán Kousal <germankousal@protonmail.com>
2023-04-19 22:10:07 -0300

    Se implemento clase OrdenAscendente con bublesort. Esta funcionando pero hay que limpiar el codigo, reordenarlo y mejorar convencion de nombres de variavbles, clases y funciones miembros.

--- 1_sort/sort.cpp
+++ 1_sort/sort.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 
+
 class Cliente
 {
 public:
@@ -14,38 +15,113 @@ public:
             arreglo[i] = rand() % 100;
         }
     };
-    // private:
+    void MostrarArreglo(void);
+    std::vector<int> *ObtenerArreglo(void);
+
+private:
     std::vector<int> arreglo;
 };
 
 class Orden
 {
 public:
-    virtual void ordenar() = 0;
+    virtual void Ordenar(std::vector<int>) = 0;
 };
 
 class OrdenAscendente : public Orden
 {
 public:
-    void ordenar();
+    void Ordenar(std::vector<int>);
+    //void Ordenar(void);
 };
 
 int main(void)
 {
+    
     time_t current_time = time(NULL);
     srand((unsigned)time(NULL));
 
-    Cliente nuevoCliente(1);
+    int longitud_arreglo;
+
+    std::cout << "Ingrese la  longitud del arreglo a ordenar: ";
+    std::cin >> longitud_arreglo;
+
+    Cliente nuevoCliente(longitud_arreglo);
+
+    /* nuevoCliente.MostrarArreglo(); */
+
     
-    for (int i=0; i<nuevoCliente.arreglo.size() ; i++)
+
+    /*
+    std::vector<int> * aux = nuevoCliente.ObtenerArreglo();
+
+    for (int i=0 ; i<longitud_arreglo;i++)
     {
-        std::cout << std::to_string(nuevoCliente.arreglo[i]) + " ";
+        std::cout << (*aux)[i] << std::endl;
     }
-    std::cout << nuevoCliente.arreglo.size();
+    */
+
+    OrdenAscendente nuevoOrden;
+    /* nuevoOrden.Ordenar(); */
+    nuevoOrden.Ordenar(*nuevoCliente.ObtenerArreglo());
     return 0;
 };
 
-void OrdenAscendente::ordenar()
+
+void Cliente::MostrarArreglo(void)
 {
-    std::cout << "\ncorrecto\n";
+    std::cout << "El arreglo de Cliente es: [";
+    for (int i = 0; i < arreglo.size(); i++)
+    {
+        std::cout << std::to_string(arreglo[i]) + " ";
+    };
+    std::cout << "]";
 }
+
+
+void OrdenAscendente::Ordenar(std::vector<int> arreglo)
+{
+    
+    int i, j, temp, pass = 0;
+    //int a[10] = {10, 2, 0, 14, 43, 25, 18, 1, 5, 45};
+    std::cout << "Input list ...\n";
+    for (i = 0; i < arreglo.size(); i++)
+    {
+        std::cout << arreglo[i] << "\t";
+    }
+    std::cout << std::endl;
+    for (i = 0; i < arreglo.size(); i++)
+    {
+        for (j = i + 1; j < arreglo.size(); j++)
+        {
+            if (arreglo[j] < arreglo[i])
+            {
+                temp = arreglo[i];
+                arreglo[i] = arreglo[j];
+                arreglo[j] = temp;
+            }
+        }
+        pass++;
+    }
+    std::cout << "Sorted Element List ...\n";
+    for (i = 0; i < arreglo.size(); i++)
+    {
+        std::cout << arreglo[i] << "\t";
+    }
+    std::cout << "\nNumber of passes taken to sort the list:" << pass << std::endl;
+    
+   //std::cout  << "OrdenAscendente" << std::endl;
+}
+
+
+
+std::vector<int> *Cliente::ObtenerArreglo(void)
+{
+    return &arreglo;
+}
+
+
+/* void OrdenAscendente::Ordenar(void)
+{
+    std::cout << "Correcto";
+} */
