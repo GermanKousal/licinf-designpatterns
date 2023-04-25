846827bfe92f19f6d3a71a00e3446c48f2b6be94
Germán Kousal <germankousal@protonmail.com>
2023-04-25 20:02:16 -0300

    Primer commit de punto 4, esqueleo basico

---
+++ 4_directories/directories.cpp
@@ -0,0 +1,27 @@
+// TP1
+// Punto 4
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Directorio
+{
+public:
+
+private:
+
+};
+
+class Archivo
+{
+public:
+
+private:
+
+};
+
+int main(void)
+{
+    return 0;
+}
