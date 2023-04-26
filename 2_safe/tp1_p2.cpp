0d0c36ca11f9319370ccf3fb9cbed3e1869ad457
Germán Kousal <germankousal@protonmail.com>
2023-04-26 19:05:44 -0300

    se agregan pausas esperando ingreso de caracter para visualizar paso a paso los cambios de estado de CajaFuerte

--- 2_safe/tp1_p2.cpp
+++ 2_safe/tp1_p2.cpp
@@ -5,22 +5,34 @@
 
 int main(void)
 {
+    char c; // para pausar entre llamadas a metodos de CajaFuerte
 
     CajaFuerte caja_fuerte;
+    std::cin >> c;
 
     caja_fuerte.configurar();
+    std::cin >> c;
     caja_fuerte.configurar();
+    std::cin >> c;
 
     caja_fuerte.desactivar();
+    std::cin >> c;
     caja_fuerte.desactivar();
+    std::cin >> c;
 
     caja_fuerte.activar();
+    std::cin >> c;
     caja_fuerte.activar();
+    std::cin >> c;
 
     caja_fuerte.desactivar();
+    std::cin >> c;
     caja_fuerte.configurar();
+    std::cin >> c;
     caja_fuerte.activar();
+    std::cin >> c;
     caja_fuerte.desactivar();
+    std::cin >> c;
 
     return 0;
 };
